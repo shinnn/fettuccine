984a4668715d1c8f31e8dd6ef7215ade773148f9
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-03-21 10:40:58 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update ESLint from v3 to v4
    * update @shinnn/eslint-config and follow its rules, for exmaple tab indent
    * switch from istanbul to nyc

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const Fettuccine = require('fettuccine-class');
 const fettuccineInstance = new Fettuccine();
 
 module.exports = function fettuccine(url, options) {
-  return fettuccineInstance.get(url, options);
+	return fettuccineInstance.get(url, options);
 };
 
 module.exports.get = module.exports;
 
 for (const method of [
-  'post',
-  'put',
-  'patch',
-  'head',
-  'delete'
+	'post',
+	'put',
+	'patch',
+	'head',
+	'delete'
 ]) {
-  module.exports[method] = (url, options) => module.exports(url, Object.assign({}, options, {
-    method: method.toUpperCase()
-  }));
+	module.exports[method] = (url, options) => module.exports(url, Object.assign({}, options, {
+		method: method.toUpperCase()
+	}));
 }
 
 module.exports.Fettuccine = Fettuccine;
--- test.js
+++ test.js
@@ -4,47 +4,53 @@ const {createServer} = require('http');
 
 const fettuccine = require('.');
 const {Fettuccine} = fettuccine;
-const {test} = require('tape');
+const test = require('tape');
 
 let responded = 0;
 
 createServer(function(req, response) {
-  if (req.method === 'POST') {
-    req.once('data', data => {
-      response.writeHead(200, {'Content-Type': 'application/json'});
-      response.end(`{"data": ${data.toString()}}`);
-    });
-  } else {
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.end('Body');
-  }
+	if (req.method === 'POST') {
+		req.once('data', data => {
+			response.writeHead(200, {'Content-Type': 'application/json'});
+			response.end(`{"data": ${data.toString()}}`);
+		});
+	} else {
+		response.writeHead(200, {'Content-Type': 'plain/text'});
+		response.end('Body');
+	}
 
-  if (++responded === 2) {
-    this.close();
-  }
+	if (++responded === 2) {
+		this.close();
+	}
 }).listen(8124);
 
 test('fettuccine()', t => {
-  t.plan(4);
+	t.plan(3);
 
-  t.equal(fettuccine.name, 'fettuccine', 'should have a function name.');
+	(async () => {
+		t.equal(
+			(await fettuccine('http://localhost:8124')).body,
+			'Body',
+			'should send a request to the server.'
+		);
+	})();
 
-  fettuccine('http://localhost:8124').then(response => {
-    t.strictEqual(response.body, 'Body', 'should send a request to the server.');
-  }).catch(t.fail);
+	(async () => {
+		t.deepEqual(
+			(await fettuccine.post('http://localhost:8124', {
+				body: [1, 2, 3],
+				json: true
+			})).body,
+			{data: [1, 2, 3]},
+			'should accept options as its second aegument.'
+		);
+	})();
 
-  fettuccine.post('http://localhost:8124', {
-    body: [1, 2, 3],
-    json: true
-  }).then(response => {
-    t.deepEqual(response.body, {data: [1, 2, 3]}, 'should accept options as its second aegument.');
-  }).catch(t.fail);
-
-  new Fettuccine().get('http://localhost:8124').then(response => {
-    t.strictEqual(
-      response.body,
-      'Body',
-      'should expose its own class `Fettuccine` as a property.'
-    );
-  }).catch(t.fail);
+	(async () => {
+		t.deepEqual(
+			(await new Fettuccine().get('http://localhost:8124')).body,
+			'Body',
+			'should expose its own class `Fettuccine` as a property.'
+		);
+	})();
 });
